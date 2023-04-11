2d9e24d443554c03353c6be61bd76df11425f15d
matgrand <matteo.grandin@gmail.com>
2023-04-12 00:04:45 +0200

    initial commit

---
+++ src/main.cpp
@@ -0,0 +1,33 @@
+#include <Arduino.h>
+#include "ota.h"
+#include "credentials.h"
+
+//set led pin
+const int ledPin = 2;
+#define DELAY 50
+
+void setup() {
+  Serial.begin(115200);
+  Serial.println("Booting");
+
+  setupOTA("ESP32-Test-OTA", ssid, pass);
+
+  // Your setup code
+
+  //set led pin as output
+  pinMode(ledPin, OUTPUT);
+
+}
+
+void loop() {
+  ArduinoOTA.handle();
+
+  // Your code here
+
+  //blink led
+  digitalWrite(ledPin, HIGH);
+  delay(DELAY);
+  digitalWrite(ledPin, LOW);
+  delay(DELAY);
+
+}
